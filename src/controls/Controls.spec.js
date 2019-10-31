c13297c880e7e2dab0ecf9ef5ba6efb2bb698c24
Clif <clifhodges13@gmail.com>
2019-10-30 20:30:59 -0500

    added snapshot test for the dashboard.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,18 @@
-// Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Controls from "./Controls";
+
+// find the lock/unlock element, then expect the open/close to not have text of open if the L/U has text 'locked'
+
+test('Gate cannot open if it\'s locked', () => {
+  const wrapper = rtl.render(<Controls />);
+
+  const lockedOrUnlocked = wrapper.getByTestId('lu')
+
+  expect(lockedOrUnlocked).toHave()
+
+  const openOrClosed = wrapper.getByTestId('oc')
+
+  expect(openOrClosed).toBeInTheDocument()
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
-// Test away
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
+
+// Shows the controls and display
+test('Dashboard renders and matches snapshot', () => {
+  const wrapper = rtl.render(<Dashboard />);
+
+  expect(wrapper.asFragment()).toMatchSnapshot();
+})
+
+// Gate defaults to unlocked and open
+test('Dashboard renders with the Display and Controls', () => {
+  const wrapper = rtl.render(<Dashboard />);
+
+  const unLocked = wrapper.getByText(/unlocked/i);
+  expect(unLocked).toBeInTheDocument();
+
+  const open = wrapper.getByText(/open/i);
+  expect(open).toBeInTheDocument();
+})
